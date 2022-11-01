292ffd17c955b04e28d58c857f3181291789f4fe
Samuel Gibson <116850189+Sdgibson7@users.noreply.github.com>
2022-10-31 22:17:45 -0700

    reorganization of project

--- scripts/data_import.m
+++ scripts/data_import.m
@@ -1,5 +1,5 @@
 % This is where we will import our data.
-% If necessary, this is also where the data will be preprocessed if it's not in the format we need
+% If necessary, this is also where the data will be preprocessed if it's not in the format/units we need
 
 % Add the data folder into the path
 addpath(".\data");
